7902d03204b5d58d74ee0f968355982f10ca5495
trendyjosh <trendyjosh@gmail.com>
2023-10-09 23:28:10 +0100

    Set themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -78,4 +79,7 @@ export default {
         },
     },
     plugins: [require("tailwindcss-animate"), require("daisyui"), forms],
+    daisyui: {
+        themes: ["light", "dark"],
+    },
 };
