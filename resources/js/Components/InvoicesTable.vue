e9c24d4170ba0e52d202c690125d04898b450705
trendyjosh <trendyjosh@gmail.com>
2023-10-10 15:04:08 +0100

    Set table row border colour

--- resources/js/Components/InvoicesTable.vue
+++ resources/js/Components/InvoicesTable.vue
@@ -20,7 +20,7 @@ defineProps({
             </thead>
             <tbody>
                 <tr
-                    class="hover:bg-gray-100 hover:dark:bg-gray-700 text-gray-800 dark:text-gray-200"
+                    class="hover:bg-gray-100 hover:dark:bg-gray-700 text-gray-800 dark:text-gray-200 border-gray-100 dark:border-gray-700"
                     v-for="invoice in invoices"
                 >
                     <td>
