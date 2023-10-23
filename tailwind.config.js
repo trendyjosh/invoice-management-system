32aaae04060d0942c040cc6008f98f313559d051
trendyjosh <trendyjosh@gmail.com>
2023-10-23 01:31:37 +0100

    Set Inter as base font

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,7 +74,7 @@ export default {
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
             fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
             },
         },
     },
