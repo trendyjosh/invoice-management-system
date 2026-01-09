32ad8e55e34857900672d8dd9d2d9a9c950c2f4f
trendyjosh <trendyjosh@gmail.com>
2026-01-09 01:12:52 +0000

    feat: dashboard chart sizing

--- resources/js/Pages/Dashboard/Partials/Charts.vue
+++ resources/js/Pages/Dashboard/Partials/Charts.vue
@@ -23,8 +23,10 @@ Chart.register(
 </script>
 
 <template>
-    <div class="stats stats-vertical lg:stats-horizontal bg-white w-full flex">
-        <div class="stat w-2/3 border-gray-100">
+    <div
+        class="stats stats-vertical lg:stats-horizontal bg-white w-full flex flex-col lg:flex-row"
+    >
+        <div class="stat lg:w-2/3 border-gray-100">
             <div class="stat-title text-primary">Line chart</div>
             <div class="stat-value text-primary">
                 <Line
@@ -47,7 +49,7 @@ Chart.register(
             </div>
         </div>
 
-        <div class="stat w-1/3 border-gray-100">
+        <div class="stat lg:w-1/3 border-gray-100">
             <div class="stat-title text-primary">Doughnut chart</div>
             <div class="stat-value text-primary">
                 <Doughnut
