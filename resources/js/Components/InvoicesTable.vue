00ece5a8d265457edf988876ec0a7f1a5c6db2da
trendyjosh <trendyjosh@gmail.com>
2026-01-17 02:47:05 +0000

    feat: apply actions to multiple invoices
    Add multiple select functionality to mass update invoices.
    
    Add status attribute to Invoice for easier display on front-end.

--- resources/js/Components/InvoicesTable.vue
+++ resources/js/Components/InvoicesTable.vue
@@ -1,19 +1,80 @@
 <script setup lang="ts">
-import { Link } from "@inertiajs/vue3";
+import { InertiaForm, Link, useForm } from "@inertiajs/vue3";
 import Table from "./Table.vue";
 import Pagination from "./Pagination.vue";
 import { LengthAwarePaginator } from "@/types/pagination";
 
-defineProps<{
+const props = defineProps<{
     invoices: LengthAwarePaginator;
 }>();
+
+const form: InertiaForm<{
+    invoices: Array<number>;
+    action: string | null;
+}> = useForm({
+    invoices: Array<number>(),
+    action: null,
+});
+
+/**
+ * Submit a list of invoices with selected action.
+ */
+function submit() {
+    form.post(route("invoices.action"), {
+        onSuccess: () => {
+            form.invoices.length = 0;
+        },
+    });
+}
+
+/**
+ * Toggle between all invoices selected or unselected.
+ */
+function toggleAll(event: Event) {
+    // Get checkbox input
+    const target = event.target as HTMLInputElement;
+    if (target.checked) {
+        // Add all bookings to selection
+        props.invoices.data.forEach((invoice) => {
+            form.invoices.push(invoice.id);
+        });
+    } else {
+        // Remove all bookings from selection
+        if (form) {
+            form.invoices.length = 0;
+        }
+    }
+}
 </script>
 
 <template>
+    <div class="bg-white table p-2" v-show="form.invoices.length">
+        <select
+            class="select select-bordered border-primary bg-white dark:bg-gray-800 disabled:bg-gray-100 mr-2"
+            name="action"
+            id="action"
+            v-model="form.action"
+        >
+            <option :value="null">Select...</option>
+            <option value="paid">Paid</option>
+            <option value="outstanding">Outstanding</option>
+        </select>
+        <button class="btn btn-outline btn-primary mr-2" @click="submit">
+            Apply
+        </button>
+        {{ form.invoices.length }} invoice(s) selected
+    </div>
     <template v-if="invoices?.data">
         <Table>
             <template #head>
                 <tr class="border-b border-gray-100 dark:border-gray-700">
+                    <th>
+                        <input
+                            title="Select all"
+                            type="checkbox"
+                            @change="toggleAll"
+                        />
+                    </th>
                     <th>ID</th>
                     <th>No.</th>
                     <th>Date</th>
@@ -28,6 +89,16 @@ defineProps<{
                     class="hover:bg-gray-100 hover:dark:bg-gray-700 text-gray-800 dark:text-gray-200 border-gray-100 dark:border-gray-700"
                     v-for="invoice in invoices.data"
                 >
+                    <td>
+                        <input
+                            type="checkbox"
+                            :class="{
+                                'border-red-500': form.errors.invoices,
+                            }"
+                            v-model="form.invoices"
+                            :value="invoice.id"
+                        />
+                    </td>
                     <td>
                         <Link
                             :href="
@@ -78,7 +149,7 @@ defineProps<{
                         </Link>
                     </td>
                     <td>
-                        {{ invoice.paid ? "Paid" : "Outstanding" }}
+                        {{ invoice.status }}
                     </td>
                     <td>
                         <a
